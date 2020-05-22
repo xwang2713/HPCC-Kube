48a7cee3eb0c9420df893050d75c593064fbc0e8
xwang2713 <xiaoming.wang@lexisnexis.com>
2020-05-22 11:35:08 -0400

    Update create_one.sh for free aws account

--- aws/EKS/eks_one.sh
+++ aws/EKS/eks_one.sh
@@ -1,1 +1,5 @@
-eksctl create cluster --name single-node-ming --version 1.15 --managed --asg-access
+NODE_TYPE="--node-type t2.micro"
+#NODE_TYPE=""
+AZ="--zones us-east-1a --zones us-east-1b"
+PROFILE="--profile ming"
+eksctl create cluster --name single-node-ming --version 1.15 --managed --asg-access ${NODE_TYPE} ${AZ} ${PROFILE}
